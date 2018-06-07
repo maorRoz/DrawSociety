45fc1514c922842d74e36e1c9aacae32970622c4
maorRoz <andory@post.bgu.ac.il>
2018-06-08 01:40:03 +0300

    added react to the asp.net project

--- DrawSociety/DrawSocietyServer/App_Start/RouteConfig.cs
+++ DrawSociety/DrawSocietyServer/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DrawSocietyServer
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Draw", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ DrawSociety/DrawSocietyServer/Scripts/react/app.js
@@ -0,0 +1,8 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h3>Hello,world!</h3>,
+    document.getElementById('root')
+
+    );
